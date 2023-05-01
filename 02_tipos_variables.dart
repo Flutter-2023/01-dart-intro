a3d59ea9bd6d2223db33ff496590014fd1fd851d
Luis Angel <angelfgdeveloper@gmail.com>
2023-05-01 17:06:00 -0500

    Tipo dynamic

--- 02_tipos_variables.dart
+++ 02_tipos_variables.dart
@@ -10,12 +10,21 @@ void main() {
 
   final sprites = <String>['ditto/front.png', 'ditto/back.png'];
 
+  // dynamic por defecto es null
+  dynamic errorMessage = 'Hola'; // No infiere el tipo
+  errorMessage = true;
+  errorMessage = [1, 2, 3, 4, 5, 6]; // Listado de numeros
+  errorMessage = {1, 2, 3, 4, 5, 6}; // set de datos
+  errorMessage = () => true;
+  errorMessage = null;
+  // errorMessage += 1; // Es un error
+
   print("""
   $pokemon
   $hp
   $isActive
   $abilities
   $sprites
-
+  $errorMessage
   """);
 }
