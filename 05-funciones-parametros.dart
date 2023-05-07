cafd5b0983e387fab53e8163eadac28036921b4b
Luis Angel <angelfgdeveloper@gmail.com>
2023-05-07 13:10:53 -0500

    funciones y paramentros

---
+++ 05-funciones-parametros.dart
@@ -0,0 +1,37 @@
+void main() {
+
+  print( greetEveryone() );
+  print( greetEveryoneArrowFunction() );
+
+  print('Suma: ${ addTwoNumbers(15, 40) }');
+  print('Suma Arrow Function: ${ addTwoNumbersWithArrowFunction(10, 20) }');
+
+  print('Suma optional: ${ addTwoNumbersOptional(15) }');
+
+
+}
+
+String greetEveryone() {
+  return 'Hello everyone';
+}
+
+String greetEveryoneArrowFunction() => 'Hello everyone with Arrow function';
+
+int addTwoNumbers(int a, int b) {
+  return a + b;
+}
+
+int addTwoNumbersWithArrowFunction(int a, int b) => a + b;
+
+// Definir b como opcional con []
+int addTwoNumbersOptional(int a, [ int? b, int c = 1 ]) {
+  // Opciones similares o equivalentes
+  // b = b ?? 0; // Verificar que b tenga un numero, sino le coloca un 0
+  // b = b + 1;
+  // b ??=0;
+  // b++;
+
+  b ??= 0;
+
+  return a + b;
+}
