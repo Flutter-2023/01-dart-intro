dc037f1aa54043c9466daf62524c4cc57143acf4
Luis Angel <angelfgdeveloper@gmail.com>
2023-05-07 14:53:23 -0500

    Extends y principio de inversion de dependencias

--- 09-classes-abstract-and-enums.dart
+++ 09-classes-abstract-and-enums.dart
@@ -4,8 +4,20 @@ void main() {
   // Las clases abstracts no pueden ser inicializadas
   // final EnergyPlant windPlant = new EnergyPlant();
 
+  final WindPlant windPlant = WindPlant(initialEnergy: 100);
 
+  print(windPlant);
+  
+  print('Wind: ${ chargePhone( windPlant ) }');
+}
 
+// Principio de inversion de dependencias
+double chargePhone( EnergyPlant plant ) {
+  if ( plant.energyLeft < 10 ) {
+    throw Exception('Not enough energy');
+  }
+
+  return plant.energyLeft - 10;
 }
 
 enum PlantType { nuclear, wind, water }
@@ -21,6 +33,24 @@ abstract class EnergyPlant {
   });
 
   void consumeEnergy(double amount);
+  // void consumeEnergy(double amount) {
+  //   throw UnimplementedError(); // Para cuando no esta implementado
+  // }
 
+}
+
+// Crear clase
+// extends o implements
+class WindPlant extends EnergyPlant {
+
+  WindPlant({
+    required double initialEnergy
+  }) : super(energyLeft: initialEnergy, type: PlantType.wind);
+
+  @override
+  void consumeEnergy(double amount) {
+    energyLeft -= amount;
+  }
+ 
 }
 
