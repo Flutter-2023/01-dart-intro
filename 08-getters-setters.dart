efe237c9d38c955e1f1295c1ff666727cd99df24
Luis Angel <angelfgdeveloper@gmail.com>
2023-05-07 14:08:59 -0500

    getters y setters

---
+++ 08-getters-setters.dart
@@ -0,0 +1,38 @@
+
+void main() {
+
+  final Square mySquare = Square(side: 10);
+
+  mySquare.side = -5;
+
+  print('Area: ${ mySquare.calculateArea() }');
+  print('Area: ${ mySquare.area }');
+
+}
+
+class Square {
+
+  // convertir a propiedad privada _side
+  double _side; // side * side
+
+  Square({ 
+    required double side 
+  }) : 
+  _side = side;
+
+  double calculateArea() {
+    return _side * _side;
+  }
+
+  double get area {
+    return _side * _side;
+  }
+
+  set side(double value) {
+    print('Setting new value $value');
+    if ( value < 0) throw 'Value must be >= 0';
+
+    _side = value; // Estableciendo nuevo valor
+  }
+
+}
